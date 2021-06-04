c2820f16ab87f9e5a107a015f01f9623c64996fa
jperezg-st <lnaokzl@gmail.com>
2021-06-04 01:40:54 -0500

    test-results

--- test/TEST1.js
+++ test/TEST1.js
@@ -1,10 +1,6 @@
-const chai = require('chai')
 const request = require('supertest')("https://test-ci-api-js.herokuapp.com")
-const chaiHttp = require('chai-http')
 const expect = require('chai').expect
-chai.use(chaiHttp)
 const nock = require('nock')
-// const url = 'http://localhost:3700'
 
 describe('Test #1', () => {
     it('should get status 200', (done) => {
@@ -23,17 +19,3 @@ describe('Test #1', () => {
         })
     })
 })
-
-
-
-
-// describe("Get all todo", () =>{
-//     it("Shoul return status 200", () => {
-//         chai.request(url)
-//         .post('/tasks')
-//         .send({name:"test 4"})
-//         .end((err, res) => {
-//             expect(res).to.have.status(200)
-//         })
-//     })
-// })
